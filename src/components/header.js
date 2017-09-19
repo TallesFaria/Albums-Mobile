0fe2c9dfe4d7a629d2ceaa2733e0918c6b42c478
TallesFaria <you@example.com>
2017-09-19 18:00:09 -0300

    Header component + Styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-  return <Text>Albums</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>  
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  textStyle: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  viewStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    height: 60,
+    paddingTop: 15,
+    marginBottom: 5,
+  },
 };
 
 export default Header;
